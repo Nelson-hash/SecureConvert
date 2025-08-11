89a201a2f4aa308168d2ddc7959591bb2f5e4b48
Nelson-hash <nlsn.remy@gmail.com>
2025-08-11 16:28:28 +0200

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,110 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+  
+  return {
+    entry: './src/index.js',
+    
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: isProduction ? '[name].[contenthash].js' : '[name].js',
+      clean: true,
+      publicPath: '/'
+    },
+    
+    devServer: {
+      static: {
+        directory: path.join(__dirname, 'public'),
+      },
+      port: 3000,
+      hot: true,
+      open: true,
+      compress: true,
+      historyApiFallback: true
+    },
+    
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader']
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif|ico)$/i,
+          type: 'asset/resource'
+        },
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/i,
+          type: 'asset/resource'
+        }
+      ]
+    },
+    
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './public/index.html',
+        favicon: './public/favicon.ico',
+        minify: isProduction ? {
+          removeComments: true,
+          collapseWhitespace: true,
+          removeRedundantAttributes: true,
+          useShortDoctype: true,
+          removeEmptyAttributes: true,
+          removeStyleLinkTypeAttributes: true,
+          keepClosingSlash: true,
+          minifyJS: true,
+          minifyCSS: true,
+          minifyURLs: true,
+        } : false
+      }),
+      
+      // Bundle analyzer for production builds
+      ...(process.env.ANALYZE ? [new BundleAnalyzerPlugin()] : [])
+    ],
+    
+    optimization: {
+      splitChunks: {
+        chunks: 'all',
+        cacheGroups: {
+          vendor: {
+            test: /[\\/]node_modules[\\/]/,
+            name: 'vendors',
+            chunks: 'all',
+          },
+          pdf: {
+            test: /[\\/]node_modules[\\/](pdfjs-dist|pdf-lib)[\\/]/,
+            name: 'pdf-libs',
+            chunks: 'all',
+          }
+        }
+      }
+    },
+    
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+        '@components': path.resolve(__dirname, 'src/components'),
+        '@converters': path.resolve(__dirname, 'src/converters'),
+        '@utils': path.resolve(__dirname, 'src/utils')
+      }
+    },
+    
+    // Prevent PDF.js worker issues
+    externals: {
+      'pdfjs-dist/build/pdf.worker.js': 'pdfjsWorker'
+    }
+  };
+};
